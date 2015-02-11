b4132ecae05b77dfd0a5384b7774bb40e933b7ac
TehMelays <Flame.teh@gmail.com>
2015-02-10 18:45:21 -0600

    Create good.py

---
+++ good.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+blob = """ﾇ;Ez{殲n捍f鰡bPFJ・ﾋ]-ﾛ2~・q｢b'髓・・｡ｹ轆ｲ・譯・,・祥ﾘ煜[ﾕ掻ｳ・ﾌ|ﾛﾊｵそｽXKL・(
+ｳg'`鞍ﾏ・(翌・ﾅ[､t晙[sﾒo ・ﾞ錂/Lg"""
+from hashlib import sha256
+print "I come in peace."
